f70f61174e5c27e8bb36ffc0b73d7421772880fe
alembicbrian <alembicbrian@gmail.com>
2024-03-24 12:59:30 +0300

    adds login and log out feature

--- apps/merchant/urls.py
+++ apps/merchant/urls.py
@@ -1,6 +1,12 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
+
 from .import views
 
 urlpatterns  = [
-    path('become_merchant/', views.become_merchant, name='become_merchant')
+    path('become_merchant/', views.become_merchant, name='become_merchant'),
+    path('merchant_admin/', views.merchant_admin, name='merchant_admin'),
+
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='merchant/login.html'), name='login'),
 ]
--- apps/merchant/views.py
+++ apps/merchant/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 
 from .models import Merchant
 
@@ -15,12 +16,17 @@ def become_merchant(request):
 
             login(request, user)
 
-            vendor = Vendor.objects.create(name=user.username, created_by=user)
+            merchant = Merchant.objects.create(name=user.username, created_by=user)
 
             return redirect('frontpage')
     else:
         form = UserCreationForm()
-    
-    return render(request, 'merchant/become_merchant.html')
+
+    return render(request, 'merchant/become_merchant.html', {'form':form}) 
+@login_required
+def merchant_admin(request):
+    merchant = request.user.merchant
+
+    return render(request, 'merchant/merchant_admin.html', {'merchant': merchant})
 
     
