90a6cb1b920cc8033304ebf91f8c596ce84f4281
alembicbrian <alembicbrian@gmail.com>
2024-03-25 00:42:34 +0300

    adds adding product feature

--- apps/merchant/urls.py
+++ apps/merchant/urls.py
@@ -6,6 +6,8 @@ from .import views
 urlpatterns  = [
     path('become_merchant/', views.become_merchant, name='become_merchant'),
     path('merchant_admin/', views.merchant_admin, name='merchant_admin'),
+    path('add-product/', views.add_product, name='add_product'),
+
 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='merchant/login.html'), name='login'),
--- apps/merchant/views.py
+++ apps/merchant/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.text import slugify
 from django.shortcuts import render, redirect
 
+
 from .models import Merchant
+from apps.product.models import Product
+
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -26,7 +31,23 @@ def become_merchant(request):
 @login_required
 def merchant_admin(request):
     merchant = request.user.merchant
+    products = merchant.products.all()
 
-    return render(request, 'merchant/merchant_admin.html', {'merchant': merchant})
+    return render(request, 'merchant/merchant_admin.html', {'merchant': merchant, 'products': products})
 
+@login_required
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            product = form.save(commit=False)
+            product.merchant = request.user.merchant
+            product.slug = slugify(product.title)
+            product.save()
+
+            return redirect('merchant_admin')
+    else:
+        form = ProductForm()
     
+    return render(request, 'merchant/add_product.html', {'form': form})
